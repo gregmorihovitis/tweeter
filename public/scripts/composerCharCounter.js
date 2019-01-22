a2dc8e1b9254e0e3b37d4adeec62936bc775b275
gregmorihovitis <gregmorihovitis@gmail.com>
2019-01-22 21:56:15 +0000

    added backspace functionality to counter

--- public/scripts/composerCharCounter.js
+++ public/scripts/composerCharCounter.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-    $('textarea').keypress(function() {
+    $('textarea').keydown(function() {
         let remaining = 140;
         const counter = $(this).siblings('.counter');
 
+        counter.css('color', 'black');
         counter.text(remaining -$(this).val().length);
+        
         if(counter.text() < 0){
             counter.css('color', 'red');
         }
